951296649ff16a3c2a40cfe163c0a9c48f872da3
halfstorm <halfstorm@nexigate.win>
2026-05-26 19:32:34 -0500

    Block vanilla double tap sprinting

--- common/src/main/java/win/demistorm/vr_swing_sprint/mixin/LocalPlayerMixin.java
+++ common/src/main/java/win/demistorm/vr_swing_sprint/mixin/LocalPlayerMixin.java
@@ -2,9 +2,11 @@ package win.demistorm.vr_swing_sprint.mixin;
 
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.vivecraft.api.VRAPI;
 import win.demistorm.vr_swing_sprint.client.SprintHelper;
@@ -12,6 +14,9 @@ import win.demistorm.vr_swing_sprint.client.SprintHelper;
 // Mixin to modify sprint behavior for VR players on servers with mod
 @Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin {
+
+    @Shadow
+    protected int sprintTriggerTime;
 
     // Invokers for private methods in LocalPlayer
     @Invoker("isSprintingPossible")
@@ -46,5 +51,25 @@ public abstract class LocalPlayerMixin {
         boolean shouldStop = !this.invokeIsSprintingPossible(player.getAbilities().flying);
 
         cir.setReturnValue(shouldStop);
+    }
+
+    // Cancel vanilla double tap sprint for VR players
+    @Inject(method = "aiStep", at = @At("HEAD"))
+    private void disableDoubleTapSprint(CallbackInfo ci) {
+        if (!SprintHelper.hasServerCapability()) {
+            return;
+        }
+
+        LocalPlayer player = (LocalPlayer)(Object)this;
+
+        try {
+            if (!VRAPI.instance().isVRPlayer(player)) {
+                return;
+            }
+        } catch (Exception e) {
+            return;
+        }
+
+        this.sprintTriggerTime = 0;
     }
 }
